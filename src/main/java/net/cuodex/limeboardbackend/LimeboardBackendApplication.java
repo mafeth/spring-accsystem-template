9c0b1c84209373059ccd03af27de7840dd838368
DevOFVictory <49123418+DevOFVictory@users.noreply.github.com>
2023-05-28 21:50:37 +0200

    Project created

---
+++ src/main/java/net/cuodex/limeboardbackend/LimeboardBackendApplication.java
@@ -0,0 +1,13 @@
+package net.cuodex.limeboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimeboardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimeboardBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/cuodex/limeboardbackend/LimeboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cuodex.limeboardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimeboardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
